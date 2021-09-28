13353e2300a4debdb293a6ba756769b6b3f6596d
Mike Tamis <tamis.mike@gmail.com>
2021-09-29 02:36:09 +1300

    Enabled introspection

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,43 +1,47 @@
-import  {  ApolloServer  }  from  "apollo-server-micro";
+import { ApolloServer } from "apollo-server-micro";
 import { NextApiRequest, NextApiResponse } from "next";
-import Cors from 'cors';
-import schema from '../../schema';
+import Cors from "cors";
+import schema from "../../schema";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 function initMiddleware(middleware: any) {
   return (req: NextApiRequest, res: NextApiResponse) =>
     new Promise((resolve, reject) => {
       middleware(req, res, (result: any) => {
         if (result instanceof Error) {
-          return reject(result)
+          return reject(result);
         }
-        return resolve(result)
-      })
-    })
-  }
+        return resolve(result);
+      });
+    });
+}
 
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: ["GET", "POST", "OPTIONS"],
   })
-)
+);
 
+const apolloServer = new ApolloServer({
+  schema,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault],
+});
 
-const  apolloServer  =  new  ApolloServer({  schema });
-
-export  const  config  =  {
-    api:  {
-        bodyParser:  false
-    }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
-
-
 
 const startServer = apolloServer.start();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   await startServer;
   await cors(req, res);
 
@@ -46,9 +50,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   })(req, res);
 }
 
-
 // const handlerPromise = apolloServer.start().then(() => apolloServer.createHandler());
-
-
 
 // export  default async (req: NextApiRequest, res: NextApiResponse) => (await handlerPromise)(req, res)

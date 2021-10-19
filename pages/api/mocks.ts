d31c517539852c7a93462ef20edefe0b9a37fc77
Gayani Mahawatte <gayani.mahawatte@gmail.com>
2021-10-19 17:08:04 +1300

    fixed formatting

--- pages/api/mocks.ts
+++ pages/api/mocks.ts
@@ -1,13 +1,12 @@
 import faker from "faker";
 
 export const mockAPI = {
-    TicketedEvent: () =>
-        ({
-            name: () => faker.random.arrayElement(["Bay Dreams", "Rhythm and Vines", "Australian Open", "Formula 1"]),
-            venue: () => faker.address.streetName(),
-            description :() => faker.random.arrayElement(["Event of the summer", "Enjoy yourself at the event", "Special guests and friends"]),
+    TicketedEvent: () => ({
+        name: () => faker.random.arrayElement(["Bay Dreams", "Rhythm and Vines", "Australian Open", "Formula 1"]),
+        venue: () => faker.address.streetName(),
+        description :() => faker.random.arrayElement(["Event of the summer", "Enjoy yourself at the event", "Special guests and friends"]),
 
-        }),
+    }),
     TicketType : () => ({
         name: () => faker.random.arrayElement(["Pre-sales", "Box-office", "General sales"]),
     }),
